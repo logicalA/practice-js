d6950e38ab292ef1c680d2fbe9484415d5e5a9e0
logicalA <144949027+logicalA@users.noreply.github.com>
2025-09-24 11:37:03 +0000

    datatypes

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,7 +1,13 @@
-"use strict" //use all JS code as newer version
+//DataTypes
+//number => 2 to power of 53    
+//bigint 
+// string => "" 
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique value
+// object => collection of key value pairs
 
-// alert ( 3 + 3) // we are using nodejs, not browser
+console.log(typeof undefined); // undefined
 
-console.log(3 + 3) // code readeablity should be high
-
-console.log("Aman") 
+console.log(typeof null); // object
