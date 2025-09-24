0d12b73a4e0fb795d93e30dd6fae6396f90a5683
logicalA <144949027+logicalA@users.noreply.github.com>
2025-09-24 11:36:26 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const account_Id = 736365;
+let account_Email = "aman123@gmail.com";
+var account_Password = "aman123";
+account_City = "Delhi";
+
+// acoount_Id = 12345 ; // This will throw an error or not allowed because account_Id is a constant
+
+account_Email = "ajaman@gmail.com";
+account_Pass = "notaman123";
+account_City = "Mumbai"; 
+
+console.log(account_Id);
+
+console.table([account_Id, account_Email, account_Password, account_City]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict" //use all JS code as newer version
+
+// alert ( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readeablity should be high
+
+console.log("Aman") 
