48457b3bd52cadb2d3b23ceb9635db39c7a94c5b
logicalA <144949027+logicalA@users.noreply.github.com>
2025-09-26 13:01:18 +0000

    commit

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("true"!="true");
+
+// console.log(2 == "5");
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+//===
+console.log(2 === "2");
