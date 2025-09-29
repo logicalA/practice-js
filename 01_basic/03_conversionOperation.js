366504e099e4aca475bb5eddf66dbf5209ba06ca
logicalA <144949027+logicalA@users.noreply.github.com>
2025-09-29 13:15:02 +0000

    Conversionoperation.js

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//*******************
+// ******operation*******
+let str1 = "Hello"
+let str2 = "Aman"
+let str3 = str1 + str2
+ 
+console.log(str3);
+
+console.log(typeof str3);   
