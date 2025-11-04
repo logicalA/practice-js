d7a5620ded08e9d3e6718878062ba8a3267e127c
logicalA <144949027+logicalA@users.noreply.github.com>
2025-11-04 09:11:22 +0000

    string manipulation

---
+++ 01_basic/05_string.js
@@ -0,0 +1,10 @@
+const name = "hitesh"
+const leetcount = 50
+console.log( name + leetcount + " is OK");//This type of concatenation is old fashioned
+
+console.log(`${name} leetcode count is ${leetcount}`);//This is the modern way of concatenation using template literals
+
+
+
+const gameName = new String('Aman')
+console.log(gameName.__proto__);//String prototype
